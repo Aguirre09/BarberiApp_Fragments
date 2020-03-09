757de9fe13c38e422a4890246af45306183d3674
Aguirre09 <alejo609@gmail.com>
2020-03-09 11:08:19 -0500

    Source code

--- app/src/main/java/com/example/barber_proe/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/barber_proe/ui/ProfileFragment.kt
@@ -9,11 +9,14 @@ import androidx.fragment.app.Fragment
 import com.example.barber_proe.Login
 import android.view.MenuInflater
 import android.view.MenuItem
-
+import android.view.View
+import android.view.ViewGroup
 import com.example.barber_proe.R
 
 import android.view.Menu
+import android.widget.Toast
 import com.example.barber_proe.MainActivity
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -26,15 +29,15 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+        val root =  inflater.inflate(R.layout.fragment_profile, container, false)
+        root.Exit.setOnClickListener {
+            val intent= Intent(context,Login::class.java)
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+            startActivity(intent)
+        }
+        return root
     }
-
-
-
-
-
-
 
 
 
